96e9fb701ff93d0465606168832689a2f58dc6e6
kidstylex <chilizhiki@gmail.com>
2021-02-24 04:58:22 +0700

    Add works

--- src/main/java/ku/message/config/SecurityConfig.java
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -1,24 +1,27 @@
 package ku.message.config;
 
-import org.springframework.context.annotation.Bean;
+import service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationService authenticationService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/home", "/css/**", "/js/**").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/home", "/signup",
+                        "/css/**", "/js/**").permitAll()
+        anyRequest().authenticated();
 
         http.formLogin()
                 .defaultSuccessUrl("/message", true)
@@ -27,16 +30,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("usa")
-                .password(encoder().encode("usa"))
-                .roles("USER");
+        auth.authenticationProvider(this.authenticationService);
+
     }
 
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
     }
-
 
 }
