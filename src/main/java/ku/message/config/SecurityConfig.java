330f1ab8aa39dc2ed5054626a310f575ad1ae330
kidstylex <chilizhiki@gmail.com>
2021-02-24 22:45:52 +0700

    Modified work factor at least 13

--- src/main/java/ku/message/config/SecurityConfig.java
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -45,6 +45,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder(12);
+        return new BCryptPasswordEncoder(13);
     }
 }
